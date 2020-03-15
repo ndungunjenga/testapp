ba883e56fd6f994f74abbe80650e7a969736ea45
Amos Njenga <njengaamos@gmail.com>
2020-03-15 13:07:15 +0300

    Removed unnecessary text in code

--- main.go
+++ main.go
@@ -1,20 +1,3 @@
-/**
- * Copyright 2017 Google Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-// [START all]
 package main
 
 import (
@@ -41,6 +24,7 @@ func main() {
 }
 
 // hello responds to the request with a plain-text "Hello, world" message.
+
 func hello(w http.ResponseWriter, r *http.Request) {
 	log.Printf("Serving request: %s", r.URL.Path)
 	host, _ := os.Hostname()
@@ -48,5 +32,3 @@ func hello(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Version: 1.0.0\n")
 	fmt.Fprintf(w, "Hostname: %s\n", host)
 }
-
-// [END all]
