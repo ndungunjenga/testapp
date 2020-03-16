029bf4ab6906ceb8f74dae69c48e41bfe03d4b10
Amos Njenga <njengaamos@gmail.com>
2020-03-16 17:03:23 +0300

    Update Hello Message

--- main.go
+++ main.go
@@ -28,7 +28,7 @@ func main() {
 func hello(w http.ResponseWriter, r *http.Request) {
 	log.Printf("Serving request: %s", r.URL.Path)
 	host, _ := os.Hostname()
-	fmt.Fprintf(w, "Hello Beth!\n")
+	fmt.Fprintf(w, "Hello People!\n")
 	fmt.Fprintf(w, "Version: 1.1.0\n")
 	fmt.Fprintf(w, "Hostname: %s\n", host)
 }
